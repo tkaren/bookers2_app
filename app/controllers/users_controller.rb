5751287f5ab7cb3cca3c47f5b2669dcf5259c09f
tsunodakaren <k6432286@gmail.com>
2021-02-24 08:02:05 +0000

    [Edit]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,29 @@
 class UsersController < ApplicationController
-  
+
  def index
-  @uesrs = User.all
+  @users = User.all
+  @user = current_user
  end
- 
+
  def show
-  @user = User.find(current_user.id)
+  @user = User.find(params[:id])
  end
- 
+
  def edit
+  @user = User.find(params[:id])
  end
- 
+
  private
- 
+
  def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
  end
- 
+
  def correct_user
   @user = User.find(params[:id])
    unless @user == current_user
     redirect_to user_path(current_user)
    end
  end
- 
+
 end
